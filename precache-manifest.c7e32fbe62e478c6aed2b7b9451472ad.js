095c43934220e793233e805275e36db50f96ea96
abeo401 <401abeo@gmail.com>
2020-03-02 17:03:57 +0900

    Updates

---
+++ precache-manifest.c7e32fbe62e478c6aed2b7b9451472ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84472b1b51ad7487b648fed8c49d4b3b",
+    "url": "/react-js-app/index.html"
+  },
+  {
+    "revision": "690ca81a4d9b55418002",
+    "url": "/react-js-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d78115ac2b843ef8c572",
+    "url": "/react-js-app/static/js/2.75b663ea.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-js-app/static/js/2.75b663ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690ca81a4d9b55418002",
+    "url": "/react-js-app/static/js/main.46d14083.chunk.js"
+  },
+  {
+    "revision": "dc919535bddc478322fe",
+    "url": "/react-js-app/static/js/runtime-main.e13819aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-js-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
